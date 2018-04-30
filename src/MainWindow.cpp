a1a7def33142a62b0258f182dbb385914411f3fc
Sohail Shafii <sohail.shafii@gmail.com>
2018-04-29 20:56:14 -0700

    make main window launchable

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,7 +1,59 @@
 #include <SDL.h>
+#include <iostream>
 
+bool initializeSDL();
+SDL_Window* createWindow(int screenWidth, int screenHeight);
+void renderLoop();
 
 int main(int argc, char* argv[]) {
 
+	if (!initializeSDL()) {
+		return 2;
+	}
+	
+	SDL_Window *window = createWindow(800, 600);
+	if (window == nullptr) {
+		return 3;
+	}
+
+	SDL_Renderer *sdlRenderer = SDL_CreateRenderer(window, -1, 0);
+	if (window == nullptr) {
+		return 3;
+	}
+
+	renderLoop();
+
 	return 0;
 }
+
+bool initializeSDL() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "Could not initialize SDL, error: " << SDL_GetError() << std::endl;
+		return false;
+	}
+	return true;
+}
+
+SDL_Window* createWindow(int screenWidth, int screenHeight) {
+	SDL_Window *window = SDL_CreateWindow("ElRey", SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+
+	if (window == nullptr) {
+		std::cerr << "Could not create window, error: " << SDL_GetError() << std::endl;
+	}
+	return window;
+}
+
+void renderLoop() {
+	SDL_Event e;
+
+	while(true) {
+
+		bool quitPressed = false;
+		while(SDL_PollEvent(&e) != 0) {
+			quitPressed = (e.type == SDL_QUIT);
+		}
+		if (quitPressed) break;
+	}
+}
+
