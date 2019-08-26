da775f9090656eb84392fd94538bbabcfb858288
Sohail Shafii <sohail.shafii@gmail.com>
2019-08-25 23:21:36 -0700

    stub out world function

--- src/WorldData/World.h
+++ src/WorldData/World.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include "Math/Primitive.h"
+
+// abstract class for all acceleration structures, or really anything
+// that represents our world
 class World {
+public:
+	World();
+	~World();
+
+	virtual void AddPrimitive(Primitive *primitive) = 0;
+	virtual void AddPrimitives(Primitive *primitives,
+		int numPrimitives) = 0;
+	// TODO: intersection function, accessors
 
 };
