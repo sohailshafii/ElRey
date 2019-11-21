b57c63679a1b1457be9fdf91e12255a4cb773bd9
Sohail Shafii <sohail.shafii@gmail.com>
2019-11-20 22:12:44 -0800

    add base camera class

---
+++ src/Cameras/Camera.cpp
@@ -0,0 +1,9 @@
+#include "Camera.h"
+
+Camera::Camera(Point4 eyePosition, Point4 lookAtPosition,
+	float horizontalFov, Vector3 up) {
+	this->eyePosition = eyePosition;
+	this->lookAtPosition = lookAtPosition;
+	this->horizontalFov = horizontalFov;
+	this->eyePosition = eyePosition;
+}
---
+++ src/Cameras/Camera.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Math/Vector3.h"
+#include "Math/Point4.h"
+
+class Camera {
+public:
+	Camera() {}
+	Camera(Point4 eyePosition, Point4 lookAtPosition,
+		float horizontalFov, Vector3 up);
+
+private:
+	Point4 eyePosition;
+	Point4 lookAtPosition;
+	float horizontalFov;
+	Vector3 up;
+
+	Vector3 forward;
+	Vector3 right;
+};
