204319ba460d4b38fb13a649ed33adfc934c682a
Sohail Shafii <sohail.shafii@gmail.com>
2020-01-14 21:11:31 -0800

    implement point light

---
+++ src/SceneData/PointLight.cpp
@@ -0,0 +1,24 @@
+#include "PointLight.h"
+
+PointLight::PointLight(const Point3 &lightPosition,
+	const Color3& radiance, float radianceScale)
+{
+	this->lightPosition = lightPosition;
+	this->radiancePreScaled = radiance*radianceScale;
+}
+
+PointLight::~PointLight()
+{
+}
+
+Vector3 PointLight::GetDirectionFromPosition(const Point3& worldPosition) 
+{
+	Vector3 directionFromPos = (lightPosition - worldPosition);
+	directionFromPos.Normalize();
+	return directionFromPos;
+}
+
+Color3 PointLight::GetRadiance() 
+{
+	return radiancePreScaled;
+}
---
+++ src/SceneData/PointLight.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "SceneData/Light.h"
+
+class PointLight : public Light
+{
+public:
+	PointLight(const Point3 &lightPosition,
+		const Color3& radiance, float radianceScale);
+	~PointLight();
+
+	virtual Vector3 GetDirectionFromPosition(
+		const Point3& worldPosition) override;
+	virtual Color3 GetRadiance() override;
+
+private:
+	Point3 lightPosition;
+	Color3 radiancePreScaled;
+};
