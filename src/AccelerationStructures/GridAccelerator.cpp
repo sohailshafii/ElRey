9467ea914f2390c84d4c34b27129c4a146c13bb3
Sohail Shafii <sohail.shafii@gmail.com>
2020-11-09 23:10:20 -0800

    start working on grid accelerator

--- src/AccelerationStructures/GridAccelerator.cpp
+++ src/AccelerationStructures/GridAccelerator.cpp
@@ -1,7 +1,44 @@
 #include "GridAccelerator.h"
 
-bool GridAccelerator::GridAccelerator() {
+GridAccelerator::GridAccelerator() {
 	
 }
 
+void GridAccelerator::SetupCells() {
+	// find min and max coordinates of the grid
+	Point3 p0 = GetMinCoordinates();
+	Point3 p1 = GetMaxCoordinates();
+	
+	// update bounding box with min and max coords
+	boundingBox.x0 = p0[0];
+	boundingBox.y0 = p0[1];
+	boundingBox.z0 = p0[2];
+	boundingBox.x1 = p1[0];
+	boundingBox.y1 = p1[1];
+	boundingBox.z1 = p1[2];
+	
+	// compute number of cells in the x, y, and z dir
+	size_t numPrimitives = primitives.size();
+	// compute grid extents in all dimensions
+	float wx = p1[0] - p0[0];
+	float wy = p1[1] - p0[1];
+	float wz = p1[2] - p0[2];
+	// use a multiplier that results in about 8x
+	// more cells than objects
+	float multiplier = 2.0;
+	float s = pow(wx * wy * wz / numPrimitives,
+				  0.333333);
+	nx = multiplier * wx / s + 1;
+	ny = multiplier * wy / s + 1;
+	nz = multiplier * wz / s + 1;
+}
 
+// TODO
+Point3 GridAccelerator::GetMinCoordinates() {
+	return Point3(0.0f, 0.0f, 0.0f);
+}
+
+// TODO
+Point3 GridAccelerator::GetMaxCoordinates() {
+	return Point3(0.0f, 0.0f, 0.0f);
+}
--- src/AccelerationStructures/GridAccelerator.h
+++ src/AccelerationStructures/GridAccelerator.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include "Math/Point3.h"
+#include "Math/AABBox.h"
+#include "Primitives/Primitive.h"
+#include <vector>
+
 class GridAccelerator {
 public:
 	
 	GridAccelerator();
 	
 private:
+	void SetupCells();
+	Point3 GetMinCoordinates();
+	Point3 GetMaxCoordinates();
+	
+	AABBox boundingBox;
+	std::vector<Primitive*> primitives;
+	int nx, ny, nz;
 };
