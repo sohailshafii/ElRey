05c317f2c7009047482a0cea91d33b7d4251c8c2
Sohail Shafii <sohail.shafii@gmail.com>
2021-03-07 21:59:37 -0800

    start working on glossy specular material

---
+++ src/Materials/GlossySpecularMaterial.cpp
@@ -0,0 +1,51 @@
+
+#include "GlossySpecularMaterial.h"
+#include "Sampling/GenericSampler.h"
+
+GlossySpecularMaterial::GlossySpecularMaterial(float ka, float kd, float ks,
+											   float exponent, const Color3& color,
+											   const Color3& ksColor, float cr, float kr) {
+	ambientBRDF.setKd(ka);
+	ambientBRDF.setCd(color);
+	diffuseBRDF.setKd(kd);
+	diffuseBRDF.setCd(color);
+	
+	glossySpecularBRDF.setKs(ks);
+	glossySpecularBRDF.setExponent(exponent);
+	glossySpecularBRDF.setCs(ksColor);
+
+	deadColor = Color::Black();
+	this->cr = cr;
+	this->kr = kr;
+}
+
+Color GlossySpecularMaterial::GetAmbientColor(const ShadingInfo& shadingInfo) const  {
+	Color3 ambientColor = ambientBRDF.GetRho(shadingInfo);
+	return Color(ambientColor[0], ambientColor[1], ambientColor[2], 1.0f);
+}
+
+Color GlossySpecularMaterial::GetDirectColor(ShadingInfo& shadingInfo) const  {
+	Color3 directColor = diffuseBRDF.F(shadingInfo);
+	Color3 specularColor = glossySpecularBRDF.F(shadingInfo);
+	Color resultColor = Color(directColor[0]+specularColor[0], directColor[1]+specularColor[1], directColor[2]+specularColor[2], 1.0f);
+
+	return resultColor;
+}
+
+Color GlossySpecularMaterial::GetColorForAreaLight(ShadingInfo& shadingInfo) const  {
+	if (shadingInfo.normalVector * shadingInfo.wo > 0.0) {
+		Color3 directColor = diffuseBRDF.F(shadingInfo);
+		Color3 specularColor = glossySpecularBRDF.F(shadingInfo);
+		return Color(directColor[0] + specularColor[0], directColor[1] + specularColor[1],
+			directColor[2] + specularColor[2], 1.0f);
+	}
+
+	return deadColor;
+}
+
+Vector3 GlossySpecularMaterial::ReflectVectorOffSurface(Vector3 const &normal,
+														Vector3 const &incomingVecFacingAwaySurface) const {
+	float ndotwo = normal * incomingVecFacingAwaySurface;
+	Vector3 reflectedVec = -incomingVecFacingAwaySurface + normal * ndotwo * 2.0f;
+	return reflectedVec.Normalized();
+}
---
+++ src/Materials/GlossySpecularMaterial.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Material.h"
+#include "Color.h"
+#include "Materials/BRDFs/LambertianBRDF.h"
+#include "Materials/BRDFs/GlossySpecularBRDF.h"
+
+class GenericSampler;
+
+class GlossySpecularMaterial : public Material {
+public:
+	GlossySpecularMaterial(float ka, float kd, float ks, float exponent,
+						   const Color3& color, const Color3& ksColor, float cr, float kr);
+
+	virtual Color GetAmbientColor(const ShadingInfo& shadingInfo) const override;
+	virtual Color GetDirectColor(ShadingInfo& shadingInfo) const override;
+	virtual Color GetColorForAreaLight(ShadingInfo& shadingInfo) const override;
+	
+	virtual bool DoesSurfaceReflect() const override {
+		return true;
+	}
+	
+	virtual Vector3 ReflectVectorOffSurface(Vector3 const &normal,
+											Vector3 const &incomingVecFacingAwaySurface) const override;
+	
+	virtual float GetReflectivity() const override {
+		return kr*cr;
+	}
+	
+private:
+	LambertianBRDF ambientBRDF;
+	LambertianBRDF diffuseBRDF;
+	GlossySpecularBRDF glossySpecularBRDF;
+
+	Color deadColor;
+	float kr, cr;
+};
+
+
