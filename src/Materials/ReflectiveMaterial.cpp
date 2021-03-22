8c91f96448858236c50a778bc1be8997a799257c
Sohail Shafii <sohail.shafii@gmail.com>
2021-03-21 17:48:07 -0700

    made reflective material inherit from phong

--- src/Materials/ReflectiveMaterial.cpp
+++ src/Materials/ReflectiveMaterial.cpp
@@ -4,59 +4,17 @@
 
 ReflectiveMaterial::ReflectiveMaterial(float ka, float kd, float ks, float exponent,
 									   const Color3& color, const Color3& ksColor,
-									   float cr, float kr) {
-	ambientBRDF.setKd(ka);
-	ambientBRDF.setCd(color);
-	diffuseBRDF.setKd(kd);
-	diffuseBRDF.setCd(color);
-	
-	glossySpecularBRDF.setKs(ks);
-	glossySpecularBRDF.setExponent(exponent);
-	glossySpecularBRDF.setCs(ksColor);
-	
+									   float cr, float kr) : PhongMaterial(ka, ka, ks, exponent, color, ksColor) {
 	perfectSpecularBRDF.setKs(ks);
 	perfectSpecularBRDF.setExponent(exponent);
 	perfectSpecularBRDF.setCs(ksColor);
 
-	deadColor = Color::Black();
 	this->reflectivity = cr*kr;
 }
 
-Color ReflectiveMaterial::GetAmbientColor(const ShadingInfo& shadingInfo) const  {
-	Color3 ambientColor = ambientBRDF.GetRho(shadingInfo);
-	return Color(ambientColor[0], ambientColor[1], ambientColor[2], 1.0f);
-}
-
-Color ReflectiveMaterial::GetDirectColor(ShadingInfo& shadingInfo) const  {
-	if (shadingInfo.normalVector * shadingInfo.wo > 0.0) {
-		Color3 directColor = diffuseBRDF.F(shadingInfo);
-		Color3 specularColor = glossySpecularBRDF.F(shadingInfo);
-		Color resultColor = Color(directColor[0]+specularColor[0], directColor[1]+specularColor[1], directColor[2]+specularColor[2], 1.0f);
-		
-		return resultColor;
-	}
-	
-	return deadColor;
-}
-
-void ReflectiveMaterial::SampleColorAndDirections(ShadingInfo &shadingInfo, std::vector<DirectionSample> & directionSamples) const  {
-	if (shadingInfo.normalVector * shadingInfo.wo > 0.0) {
-		Vector3 diffuseWi, specularWi;
-		float diffusePdf, specularPdf;
-		Color3 diffuseColor = diffuseBRDF.SampleF(shadingInfo, diffusePdf, diffuseWi);
-		Color3 specularColor = glossySpecularBRDF.SampleF(shadingInfo, specularPdf, specularWi);
-		directionSamples.push_back(DirectionSample(Color(diffuseColor[0], diffuseColor[1], diffuseColor[2], 1.0f), diffusePdf, diffuseWi));
-		directionSamples.push_back(DirectionSample(Color(specularColor[0], specularColor[1], specularColor[2], 1.0f), specularPdf, specularWi));
-	}
-
-	return deadColor;
-}
-
 void ReflectiveMaterial::SetSampler(GenericSampler *sampler) {
-	ambientBRDF.setSampler(sampler);
-	diffuseBRDF.setSampler(sampler->clone());
-	glossySpecularBRDF.setSampler(sampler->clone());
-	perfectSpecularBRDF.setSampler(sampler->clone());
+	PhongMaterial::SetSampler(sampler->clone());
+	perfectSpecularBRDF.setSampler(sampler);
 }
 
 void ReflectiveMaterial::GetSecondaryVectors(Vector3 const &normal,
--- src/Materials/ReflectiveMaterial.h
+++ src/Materials/ReflectiveMaterial.h
@@ -2,20 +2,15 @@
 
 #include "Material.h"
 #include "Color.h"
-#include "Materials/BRDFs/LambertianBRDF.h"
-#include "Materials/BRDFs/GlossySpecularBRDF.h"
+#include "Materials/PhongMaterial.h"
 #include "Materials/BRDFs/PerfectSpecularBRDF.h"
 
 class GenericSampler;
 
-class ReflectiveMaterial : public Material {
+class ReflectiveMaterial : public PhongMaterial {
 public:
 	ReflectiveMaterial(float ka, float kd, float ks, float exponent, const Color3& color,
 					   const Color3& ksColor, float cr, float kr);
-
-	virtual Color GetAmbientColor(const ShadingInfo& shadingInfo) const override;
-	virtual Color GetDirectColor(ShadingInfo& shadingInfo) const override;
-	virtual void SampleColorAndDirections(ShadingInfo &shadingInfo, std::vector<DirectionSample> & directionSamples) const override;
 	
 	virtual void SetSampler(class GenericSampler *sampler) override;
 	
@@ -24,12 +19,7 @@ public:
 									 std::vector<SecondaryVectorInfo> & secondaryVectors) const override;
 	
 private:
-	LambertianBRDF ambientBRDF;
-	LambertianBRDF diffuseBRDF;
-	GlossySpecularBRDF glossySpecularBRDF;
 	PerfectSpecularBRDF perfectSpecularBRDF;
-
-	Color deadColor;
 	float reflectivity;
 };
 
