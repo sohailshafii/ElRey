783c7ad9d5154eccef2eb40563bd435388ed41fc
Sohail Shafii <sohail.shafii@gmail.com>
2021-03-29 21:01:03 -0700

    use white color for reflective vector

--- src/Materials/ReflectiveMaterial.cpp
+++ src/Materials/ReflectiveMaterial.cpp
@@ -26,5 +26,5 @@ void ReflectiveMaterial::GetSecondaryVectors(ShadingInfo const & shadingInfo,
 		perfectSpecularBRDF.GetReflectionVector(shadingInfo.wo,
 												shadingInfo.normalVector);
 	secondaryVectors.push_back(SecondaryVectorInfo(reflectedVec, reflectivity,
-												   GetDirectColor(shadingInfo)));
+												   Color::White()));
 }
